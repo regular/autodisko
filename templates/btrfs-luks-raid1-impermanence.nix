eab7acffdbf6c244b1accabe9c8188f41d4f7eb1
regular <jan@lagomorph.de>
2025-02-07 15:40:53 +0100

    Fix double assignment to `boot`

--- templates/btrfs-luks-raid1-impermanence.nix
+++ templates/btrfs-luks-raid1-impermanence.nix
@@ -2,44 +2,65 @@
 let
   disks = config.fileSystems;
   hasMirroredBoot = disks ? "/boot" && disks ? "/boot2";
+  disk1 = config.disko.devices.disk.disk1.device;
+  disk2 = config.disko.devices.disk.disk2.device;
 in {
   # For impermanence
-  boot.initrd.systemd.services.rollback = {
-    description = "Rollback BTRFS root subvolume to a pristine state";
-    unitConfig.DefaultDependencies = "no";
-    serviceConfig.Type = "oneshot";
-    wantedBy = [ "initrd.target" ];
-    after = if hasMirroredBoot 
-      then [ "systemd-cryptsetup@crypted1.service" "systemd-cryptsetup@crypted2.service" ]
-      else [ "systemd-cryptsetup@crypted.service" ];
-    before = [ "sysroot.mount" ];
+  boot = {
+    loader = {
+      grub = lib.mkIf (config.boot.loader.grub.enable && hasMirroredBoot) {
+        devices = [ disk1 disk2 ];
+        mirroredBoots = [
+          { path = "/boot"; devices = [disk1]; }
+          { path = "/boot2"; devices = [disk2]; }
+        ];
+      };
+      
+      systemd-boot = lib.mkIf (config.boot.loader.systemd-boot.enable && hasMirroredBoot) {
+        mirroredBoots = [
+          { path = "/boot"; }
+          { path = "/boot2"; }
+        ];
+      };
+    };
 
-    script = ''
-      vgchange -ay pool
-      mkdir -p /btrfs_tmp
-      mount /dev/pool/root /btrfs_tmp
+    initrd.systemd.services.rollback = {
+      description = "Rollback BTRFS root subvolume to a pristine state";
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      wantedBy = [ "initrd.target" ];
+      after = if hasMirroredBoot 
+        then [ "systemd-cryptsetup@crypted1.service" "systemd-cryptsetup@crypted2.service" ]
+        else [ "systemd-cryptsetup@crypted.service" ];
+      before = [ "sysroot.mount" ];
 
-      if [[ -e /btrfs_tmp/root ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      fi
+      script = ''
+        vgchange -ay pool
+        mkdir -p /btrfs_tmp
+        mount /dev/pool/root /btrfs_tmp
 
-      delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
-          done
-          btrfs subvolume delete "$1"
-      }
+        if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+        fi
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+        }
 
-      btrfs subvolume create /btrfs_tmp/root
-      umount /btrfs_tmp
-    '';
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
+        done
+
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    };
   };
 
   fileSystems = {
@@ -149,28 +170,6 @@ in {
             };
           };
         };
-      };
-    };
-  };
-
-  boot = let
-    disk1 = config.disko.devices.disk.disk1.device;
-    disk2 = config.disko.devices.disk.disk2.device;
-  in {
-    loader = {
-      grub = lib.mkIf (config.boot.loader.grub.enable && hasMirroredBoot) {
-        devices = [ disk1 disk2 ];
-        mirroredBoots = [
-          { path = "/boot"; devices = [disk1]; }
-          { path = "/boot2"; devices = [disk2]; }
-        ];
-      };
-      
-      systemd-boot = lib.mkIf (config.boot.loader.systemd-boot.enable && hasMirroredBoot) {
-        mirroredBoots = [
-          { path = "/boot"; }
-          { path = "/boot2"; }
-        ];
       };
     };
   };

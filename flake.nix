40dd81a3ddcbb96b84a33c0429e1ec4572a8e4b4
regular <jan@lagomorph.de>
2025-02-10 15:54:01 +0100

    Run disko instead of disko-install (again)

--- flake.nix
+++ flake.nix
@@ -67,9 +67,10 @@
           ${pkgs.gnutar}/bin/tar -xzf /tmp/flake.tar.gz --strip-components=1 -C /tmp/flake
           nixos-generate-config --show-hardware-config --no-filesystems --root /mnt > /tmp/flake/hardware/$conf.nix
           disk_args=$(echo "$disk_devices" | gawk -F',' '{for(i=1;i<=NF;i++) printf "--disk disk%d /dev/%s ", i, $i}')
-          mkdir -p /tmp/mnt
-          chmod 755 /tmp/mnt
-          ${disko.packages.${system}.default}/bin/disko-install --mount-point /tmp/mnt --write-efi-boot-entries --flake /tmp/flake\#$conf $disk_args
+          #mkdir -p /tmp/mnt
+          #chmod 755 /tmp/mnt
+          #${disko.packages.${system}.default}/bin/disko-install --mount-point /tmp/mnt --write-efi-boot-entries --flake /tmp/flake\#$conf $disk_args
+          ${disko.packages.${system}.default}/bin/disko --no-deps --mode destroy,format,mount --mount-point /mnt --flake /tmp/flake\#$conf --yes-wipe-all-disks
         else
           ${disko.packages.${system}.default}/bin/disko --mode disko /tmp/disk-config.nix
           mount
